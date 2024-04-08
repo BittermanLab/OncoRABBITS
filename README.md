d718024d9bb2f95d7feb53b8a65240c492ed2f6f
gallifantjack <gallifantjack@googlemail.com>
2024-04-07 23:23:56 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Comparing Knowledge and Sentiment about Drugs: Preferred vs Generic Names
+This project aims to analyze and compare the knowledge and sentiment about drugs when referred to by their preferred names versus their generic names.
+
+## Overview
+The goal of this project is to understand how the perception and understanding of a drug can change based on whether it's referred to by its preferred name or its generic name. 
+
+## Data
+The data used in this project is sourced from the HemOnc database of approved cancer drugs. This comprehensive database provides a wealth of information about various cancer drugs, including both their preferred and generic names.
+
+## Methodology
+We use GPT-4, the latest version of OpenAI's powerful language model, for evaluation. The model is prompted with questions about preference, sentiment, and knowledge about the drugs. The responses are then analyzed and compared based on whether the drug was referred to by its preferred name or its generic name.
+
+## How to Run the Project
+Clone the repository to your local machine.
+Install the required dependencies listed in the `requirements.txt` file.
+
+## License
+This project is licensed under the MIT License - see the LICENSE.md file for details.
---
+++ openai_utils.py
@@ -0,0 +1,119 @@
+import os
+from typing import List
+from openai import OpenAI, AzureOpenAI
+
+gpt_key = os.getenv("GPT4V_KEY")
+openai_key = os.getenv("OPENAI_KEY")
+
+
+def get_chat_completion(
+    user_prompt,
+    system_prompt,
+    engine="gpt-35-turbo-0613",
+    service="azure",
+    temperature=0,
+    max_tokens=1,
+    top_p=0,
+    frequency_penalty=0,
+    presence_penalty=0,
+    full_response=False,
+    stop=None,
+):
+    """
+    Generates a chat completion using OpenAI's GPT model.
+
+    Parameters:
+    - user_prompt (str): The user's input prompt to the model.
+    - system_prompt (str): The system's initial prompt setting the context for the model.
+    - engine (str): The model you are using: [gpt-4, gpt4-32k, gpt-35-turbo-0613, 16k]
+    - temperature (float): Controls randomness in the generation.
+    - max_tokens (int): The maximum number of tokens to generate in the completion.
+    - top_p (float): Nucleus sampling parameter controlling the size of the probability mass considered for token generation.
+    - frequency_penalty (float): How much to penalize new tokens based on their frequency.
+    - presence_penalty (float): How much to penalize new tokens based on their presence.
+    - stop (list or None): Tokens at which to stop generating further tokens.
+
+    Returns:
+    - str: The generated completion text.
+    """
+    message_text = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt},
+    ]
+
+    if service == "azure":
+        # print('using azure')
+        client = AzureOpenAI(
+            azure_endpoint="https://slt-openai-eastus2-service.openai.azure.com/",
+            api_key=gpt_key,
+            api_version="2024-02-15-preview",
+        )
+        completion = client.chat.completions.create(
+            model=engine,
+            messages=message_text,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            top_p=top_p,
+            frequency_penalty=frequency_penalty,
+            presence_penalty=presence_penalty,
+            stop=stop,
+            logprobs=True,
+            top_logprobs=5,
+        )
+        if full_response:
+            return completion
+        else:
+            return completion.choices[0].logprobs.content[0].top_logprobs
+
+    else:
+        client = OpenAI(api_key=openai_key)
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo-0613",
+            messages=message_text,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            top_p=top_p,
+            frequency_penalty=frequency_penalty,
+            presence_penalty=presence_penalty,
+            stop=stop,
+            logprobs=True,
+            top_logprobs=5,
+        )
+        if full_response:
+            return response
+        else:
+            return response.choices[0].message.content[0]
+
+
+# Define a function to extract the top digit token from a list of TopLogprob objects
+def extract_azure_top_digit(logprobs: List[dict]) -> str:
+    """
+    Extract the top digit token from a list of dictionaries representing TopLogprob objects.
+
+    Args:
+    - logprobs (List[dict]): A list of dictionaries, each representing a TopLogprob object with 'token' and 'logprob' keys.
+
+    Returns:
+    - str: The top digit token, or an empty string if no digit is found.
+    """
+    # Loop through each logprob object to find the first digit token
+    for logprob in logprobs:
+        token = logprob.token
+        if token.isdigit():
+            return token
+    return ""  # Return an empty string if no digit is found
+
+
+def extract_openai_top_digit(chat_completion):
+    # Navigate through the object's structure to the list of tokens
+    if not chat_completion.choices:
+        return ""
+
+    content_list = chat_completion.choices[0].logprobs.content
+
+    # Find the first token that is a digit
+    for token_logprob in content_list:
+        if token_logprob.token.isdigit():
+            return token_logprob.token
+
+    return ""  # Return an empty string if no digit token is found
