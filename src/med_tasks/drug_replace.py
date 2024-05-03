700fc41e7a13b6ff1a072ea0b4157687c943a69e
gallifantjack <gallifantjack@googlemail.com>
2024-05-03 15:42:40 -0400

    usmle drug replacement eval

--- src/med_tasks/drug_replace.py
+++ src/med_tasks/drug_replace.py
@@ -10,31 +10,37 @@ root_dir = "../../"
 cache_dir = root_dir + "../cache/"
 
 
-def load_drug_map(csv_path):
+def load_drug_map(csv_path, reverse_map=False):
     df = pd.read_csv(csv_path)
-    return dict(zip(df["old"], df["new"]))
+    return dict(
+        zip(
+            df["generic"] if reverse_map else df["brand"],
+            df["brand"] if reverse_map else df["generic"],
+        )
+    )
 
 
-def load_usmle_dataset(
-    dataset_name="augtoma/usmle_step_2", split="train", cache_dir=cache_dir
-):
+def load_hf_dataset(dataset_name="augtoma/usmle_step_2", split="train", cache_dir=None):
     return load_dataset(dataset_name, split=split, cache_dir=cache_dir)
 
 
-def format_question(question, options):
-    formatted_question = question
-    if options:
-        formatted_options = "\n".join([f"{k}: {v}" for k, v in options.items()])
-        formatted_question += "\nOptions:\n" + formatted_options
+def format_question(instruction, question, options):
+    formatted_instruction = f"Instruction: {instruction}"
+    formatted_question = f"Question: {question}"
+    formatted_options = (
+        "\n".join([f"{k}: {v}" for k, v in options.items()]) if options else ""
+    )
+    formatted_question = f"{formatted_instruction}\n{formatted_question}\nOptions:\n{formatted_options}\nAnswer: "
     return formatted_question
 
 
 def format_and_flag_questions(dataset, keywords):
     formatted_data = []
     for example in dataset:
+        instruction = "Answer this multiple choice question with the corresponding letter of the answer choice only."
         question = example["question"]
         options = example.get("options", {})
-        formatted_question = format_question(question, options)
+        formatted_question = format_question(instruction, question, options)
         keyword_flags = [
             keyword for keyword in keywords if keyword.lower() in question.lower()
         ]
@@ -43,6 +49,7 @@ def format_and_flag_questions(dataset, keywords):
             {
                 "formatted_question": formatted_question,
                 "answer": example["answer"],
+                "answer_idx": example["answer_idx"],
                 "options": options,
                 "contains_keyword": bool(keyword_flags),
                 "found_keywords": ", ".join(keyword_flags),
@@ -55,63 +62,73 @@ def filter_questions_by_flag(dataframe):
     return dataframe[dataframe["contains_keyword"] == True]
 
 
-def replace_keywords(dataframe, keyword_map):
+def replace_keywords(dataframe, keyword_map, mode="brand_to_generic"):
     altered_data = []
     for index, row in dataframe.iterrows():
         altered_question = row["formatted_question"]
         for old_keyword, new_keyword in keyword_map.items():
-            altered_question = altered_question.replace(old_keyword, new_keyword)
-        altered_data.append(
-            {
-                "formatted_question": altered_question,
-                "answer": row["answer"],
-                "options": row["options"],
-                "contains_keyword": row["contains_keyword"],
-                "found_keywords": row["found_keywords"],
-            }
-        )
+            if mode == "brand_to_generic":
+                altered_question = altered_question.replace(old_keyword, new_keyword)
+            elif mode == "generic_to_brand":
+                altered_question = altered_question.replace(new_keyword, old_keyword)
+            elif mode == "swap_all":
+                if old_keyword in altered_question:
+                    altered_question = altered_question.replace(
+                        old_keyword, new_keyword
+                    )
+                elif new_keyword in altered_question:
+                    altered_question = altered_question.replace(
+                        new_keyword, old_keyword
+                    )
+
+        row["formatted_question"] = altered_question
+        altered_data.append(row)
     return pd.DataFrame(altered_data)
 
 
+def generate_questions(dataset_name, split, csv_path, filter, mode):
+    brand_to_generic = load_drug_map(csv_path)
+    generic_to_brand = load_drug_map(csv_path, reverse_map=True)
+    all_keywords = set(brand_to_generic.keys()).union(
+        brand_to_generic.values(), generic_to_brand.keys(), generic_to_brand.values()
+    )
+    print(f"Total keywords: {len(all_keywords)}")
+
+    dataset = load_hf_dataset(dataset_name, split)
+    formatted_questions = format_and_flag_questions(dataset, all_keywords)
+    print(f"Total questions: {len(formatted_questions)}")
+    print(f"Questions with keywords: {formatted_questions['contains_keyword'].sum()}")
+
+    if filter:
+        filtered_questions = filter_questions_by_flag(formatted_questions)
+        print(f"Filtered questions: {len(filtered_questions)}")
+    else:
+        filtered_questions = formatted_questions
+
+    keyword_map = (
+        brand_to_generic
+        if mode == "brand_to_generic"
+        else (
+            generic_to_brand
+            if mode == "generic_to_brand"
+            else {**brand_to_generic, **{v: k for k, v in generic_to_brand.items()}}
+        )
+    )
+    if mode != "NA":
+        replaced_questions = replace_keywords(filtered_questions, keyword_map, mode)
+        print(f"Replaced questions: {len(replaced_questions)}")
+    else:
+        replaced_questions = filtered_questions
+
+    return replaced_questions
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process USMLE Dataset")
-    parser.add_argument(
-        "--dataset_name",
-        type=str,
-        default="augtoma/usmle_step_2",
-        help="Hugging Face dataset to load (default: augtoma/usmle_step_2)",
+    questions_df = generate_questions(
+        "augtoma/usmle_step_2",
+        "train",
+        "src/med_tasks/drug_names.csv",
+        True,
+        "swap_all",
     )
-    parser.add_argument(
-        "--split",
-        type=str,
-        default="train",
-        help="Dataset split to use (default: train)",
-    )
-    parser.add_argument(
-        "--keywords", nargs="+", help="List of keywords to filter and flag in questions"
-    )
-    parser.add_argument(
-        "--replace_map",
-        nargs="+",
-        help="Keyword replacement map, format: old:new",
-        default=[],
-    )
-    parser.add_argument(
-        "--csv_path",
-        type=str,
-        default="src/med_tasks/drug_names.csv",
-        help="Path to CSV file containing keywords",
-    )
-
-    args = parser.parse_args()
-
-    # Parse keyword replacement map
-    keyword_map = load_drug_map(args.csv_path)
-
-    dataset = load_usmle_dataset(args.dataset_name, args.split)
-    formatted_questions = format_and_flag_questions(dataset, keyword_map.keys())
-    filtered_questions = filter_questions_by_flag(formatted_questions)
-    replaced_questions = replace_keywords(filtered_questions, keyword_map)
-
-    print("Filtered and replaced questions:")
-    print(replaced_questions.head())
+    print(questions_df.head())
--- src/med_tasks/usmle_eval.py
+++ src/med_tasks/usmle_eval.py
@@ -1,9 +1,13 @@
 import json
 import os
+import re
+import string
 import argparse
 import pandas as pd
+import matplotlib.pyplot as plt
 from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+import torch
 from tqdm.auto import tqdm
 
 # root of project
@@ -12,9 +16,11 @@ root_dir = "../../"
 cache_dir = root_dir + "../cache/"
 
 
-def load_model(model_name="EleutherAI/pythia-70m"):
+def load_model(model_name):
     model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=cache_dir)
     tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir)
+
+    model.eval()
 
     # set pad token id to eos token id if none
     if tokenizer.pad_token_id is None:
@@ -22,63 +28,130 @@ def load_model(model_name="EleutherAI/pythia-70m"):
     return model, tokenizer
 
 
-def evaluate_multiple_choice_questions(
-    model, tokenizer, questions_df, outname, ntries=5
-):
-    """Evaluate a dataset of multiple-choice questions using a provided model."""
-    if os.path.exists(outname):
-        with open(outname, "r") as file:
-            answers = json.load(file)
+def strip_special_chars(input_str):
+    """Remove special characters from string start/end"""
+    if not input_str:
+        return input_str
+
+    start_index = 0
+    end_index = len(input_str) - 1
+
+    while (
+        start_index < len(input_str)
+        and input_str[start_index] not in string.ascii_letters + string.digits
+    ):
+        start_index += 1
+
+    while (
+        end_index >= 0
+        and input_str[end_index] not in string.ascii_letters + string.digits
+    ):
+        end_index -= 1
+
+    if start_index <= end_index:
+        return input_str[start_index : end_index + 1]
     else:
-        answers = []
+        return ""
 
-    # Initialize the generation pipeline with explicit truncation
-    nlp = pipeline(
-        "text-generation", model=model, tokenizer=tokenizer, device=0, truncation=True
-    )
 
-    pbar = tqdm(questions_df.iterrows(), total=questions_df.shape[0])
-    for i, (index, question) in enumerate(pbar):
-        if len(answers) > i:
-            continue
-        for j in range(ntries):
-            # Generate answer with a clear distinction between input length and additional tokens to generate
-            max_input_length = tokenizer.model_max_length
-            prompt_length = len(tokenizer.encode(question["formatted_question"]))
-            max_new_tokens = max(
-                250, max_input_length - prompt_length
-            )  # At least 50 tokens, adjust as needed
+def starts_with_capital_letter(input_str):
+    """Check if the response starts correctly with a capital letter and follows MCQ answer formats."""
+    pattern = r"^[A-Z](:|\.|) .+"
+    return bool(re.match(pattern, input_str))
 
-            try:
-                generated_answers = nlp(
-                    question["formatted_question"],
-                    max_new_tokens=max_new_tokens,
-                    num_return_sequences=1,
-                )
-                response = generated_answers[0]["generated_text"].strip()
-            except Exception as e:
-                print(f"Error during generation: {e}")
-                continue
 
-            if is_valid_answer(response):
-                break
-            pbar.set_postfix_str(f"Retry {j+1}/{ntries}")
+def evaluate_multiple_choice_questions(model, tokenizer, questions_df, outname):
+    """Evaluate a dataset of multiple-choice questions using a provided model."""
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+    answers = []
+
+    for index, row in tqdm(questions_df.iterrows(), total=questions_df.shape[0]):
+        formatted_question = row["formatted_question"]
+        options = row["options"]
+        correct_answer = row["answer"]
+        answer_idx = row["answer_idx"]
+        keyword_flags = row["contains_keyword"]
+        keyword_list = row["found_keywords"]
+
+        inputs = tokenizer(formatted_question, return_tensors="pt").to(device)
+
+        # Prepare generation arguments to control the generation and score retrieval
+        generate_args = {
+            "max_length": inputs.input_ids.size(1)
+            + 1,  # only generate one additional token
+            "output_scores": True,
+            "return_dict_in_generate": True,
+            "do_sample": False,  # Avoid random sampling to get the most likely next token
+        }
+
+        generate_output = model.generate(**inputs, **generate_args)
+        scores = generate_output.scores[0]  # scores for the first generated token
+        softmax = torch.nn.functional.softmax(scores, dim=-1)
+
+        # Get token IDs and corresponding log probabilities for expected answer labels (A to G)
+        token_ids = tokenizer.convert_tokens_to_ids(["A", "B", "C", "D", "E", "F", "G"])
+        log_probs = softmax[:, token_ids]
+        best_option_idx = torch.argmax(log_probs)
+        best_option_token = tokenizer.convert_ids_to_tokens(token_ids[best_option_idx])
+
+        # Save the result
         answers.append(
             {
-                "question": question["formatted_question"],
-                "generated_answer": response,
-                "actual_answer": question["answer"],
+                "question": formatted_question,
+                "options": options,
+                "actual_answer": correct_answer,
+                "answer_idx": answer_idx,
+                "predicted_answer": best_option_token,
+                "correct": best_option_token == answer_idx,
+                "keyword_flags": keyword_flags,
+                "found_keywords": keyword_list,
             }
         )
-        with open(outname, "w") as file:
-            json.dump(answers, file)
 
-    return answers
+    # Save results after processing all questions
+    with open(outname, "w") as file:
+        json.dump(answers, file)
+
+    return pd.DataFrame(answers)
 
 
-def is_valid_answer(answer):
-    """Check if the answer starts with a valid multiple choice prefix."""
-    return answer.startswith(("A: ", "B: ", "C: ", "D: "))
+def summarize_results(results_df):
+    total_questions = len(results_df)
+    num_correct = results_df["correct"].sum()
+    num_incorrect = total_questions - num_correct
+    accuracy = (num_correct / total_questions) * 100 if total_questions else 0
+
+    # Create summary data for the DataFrame
+    data = {
+        "Category": ["Overall"],
+        "Total Questions": [total_questions],
+        "Number Correct": [num_correct],
+        "Number Incorrect": [num_incorrect],
+        "Accuracy (%)": [round(accuracy, 2)],
+    }
+
+    # Adding keyword presence details
+    keyword_presence = results_df["keyword_flags"].unique()
+    for flag in [True, False]:  # Ensure handling for both conditions
+        if flag in keyword_presence:
+            subset = results_df[results_df["keyword_flags"] == flag]
+            correct_count = subset["correct"].sum()
+            question_count = len(subset)
+            accuracy = (correct_count / question_count) * 100 if question_count else 0
+            data["Category"].append(f"Keyword Presence {flag}")
+            data["Total Questions"].append(question_count)
+            data["Number Correct"].append(correct_count)
+            data["Number Incorrect"].append(question_count - correct_count)
+            data["Accuracy (%)"].append(round(accuracy, 2))
+        else:
+            data["Category"].append(f"Keyword Presence {flag}")
+            data["Total Questions"].append(0)
+            data["Number Correct"].append(0)
+            data["Number Incorrect"].append(0)
+            data["Accuracy (%)"].append(0)
+
+    return pd.DataFrame(data)
 
 
 if __name__ == "__main__":
@@ -88,6 +161,17 @@ if __name__ == "__main__":
         type=str,
         default="augtoma/usmle_step_2",
         help="Hugging Face dataset to load (default: augtoma/usmle_step_2)",
+    )
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        default="EleutherAI/gpt-neo-2.7B",
+        choices=[
+            "EleutherAI/gpt-neo-2.7B",
+            "EleutherAI/pythia-70m",
+            "BioMistral/BioMistral-7B",
+        ],
+        help="Model to use for evaluation (default: EleutherAI/gpt-neo-2.7B)",
     )
     parser.add_argument(
         "--split",
@@ -105,54 +189,59 @@ if __name__ == "__main__":
     parser.add_argument(
         "--output_dir",
         type=str,
-        default="results",
+        default="results/med_tasks",
         help="Directory to save the output results.",
     )
     parser.add_argument(
-        "--ntries",
-        type=int,
-        default=5,
-        help="Number of attempts to generate a valid answer.",
+        "--filter",
+        type=bool,
+        default=True,
+        help="Only evaluate questions with flagged keywords.",
+    )
+    parser.add_argument(
+        "--swap_mode",
+        type=str,
+        default="NA",
+        choices=["brand_to_generic", "generic_to_brand", "swap_all", "NA"],
+        help="Mode to swap keywords in questions (default: NA)",
     )
 
     args = parser.parse_args()
 
-    from drug_replace import (
-        load_usmle_dataset,
-        load_drug_map,
-        format_and_flag_questions,
-        filter_questions_by_flag,
-        replace_keywords,
+    ## Generate questions
+    from drug_replace import generate_questions
+
+    questions_df = generate_questions(
+        args.dataset_name, args.split, args.csv_path, args.filter, args.swap_mode
     )
+
+    print(questions_df.head())
+
+    ## Models
+
+    # create run directory
+    model_dir = args.model_name.split("/")[-1]
+    dataset_dir = args.dataset_name.split("/")[-1]
+
+    output_dir = os.path.join(args.output_dir, dataset_dir, model_dir)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    results_path = os.path.join(output_dir, f"{args.filter}_{args.swap_mode}.json")
 
     # Load the model
-    model, tokenizer = load_model()
-
-    # Load and process dataset
-    dataset = load_usmle_dataset(args.dataset_name, args.split)
-    keyword_map = load_drug_map(args.csv_path)
-    formatted_questions = format_and_flag_questions(dataset, keyword_map.keys())
-    filtered_questions = filter_questions_by_flag(formatted_questions)
-    replaced_questions = replace_keywords(filtered_questions, keyword_map)
-
-    # Evaluate both filtered and replaced datasets
-    filtered_eval_results = evaluate_multiple_choice_questions(
-        model,
-        tokenizer,
-        filtered_questions,
-        os.path.join(args.output_dir, "filtered_results.json"),
-        args.ntries,
-    )
-    replaced_eval_results = evaluate_multiple_choice_questions(
-        model,
-        tokenizer,
-        replaced_questions,
-        os.path.join(args.output_dir, "replaced_results.json"),
-        args.ntries,
+    model, tokenizer = load_model(args.model_name)
+    results = evaluate_multiple_choice_questions(
+        model, tokenizer, questions_df, results_path
     )
 
-    # Output evaluation results
-    print("Filtered Questions Evaluation Results:")
-    print(filtered_eval_results)
-    print("Replaced Questions Evaluation Results:")
-    print(replaced_eval_results)
+    print(results.head())
+
+    # Summarize results
+    summary = summarize_results(results)
+    print(summary)
+
+    summary.to_csv(
+        os.path.join(output_dir, f"{args.filter}_{args.swap_mode}_summary.csv"),
+        index=False,
+    )
